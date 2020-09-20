0b49656da03d993428301252cc827e7c336e7c1d
Sujan Basnet <sujanbasnyet334@gmail.com>
2020-09-20 21:55:52 +0545

    done and dusted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="container" style={{ marginTop: "80px" }}>
+    <div className="row">
+      <div className="col-md-8 m-auto">
+        <h1 className="display-4 ">Contact Form</h1>
+        <Form />
+      </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Input from "./form/Input";
+import TextArea from "./form/TextArea";
+import axios from "axios";
+import config from "../config.json";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const { email, name, message, phone } = formData;
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(config.apiUrl);
+    console.log(formData);
+    const response = await axios.post(`${config.apiUrl}`, {
+      name,
+      email,
+      phone,
+      message,
+    });
+    console.log(response);
+    console.log(formData);
+  };
+  return (
+    <form className="form" noValidate onSubmit={onSubmit}>
+      <Input
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChange}
+        placeholder="Name"
+      />
+      <Input
+        type="email"
+        name="email"
+        value={email}
+        onChange={onChange}
+        placeholder="Email Address"
+      />
+      <Input
+        type="text"
+        name="phone"
+        value={phone}
+        onChange={onChange}
+        placeholder="Contact Number"
+      />
+      <TextArea
+        type="text"
+        name="message"
+        value={message}
+        onChange={onChange}
+        placeholder="Message Here"
+        rows="7"
+      />
+      <button className="btn btn-dark btn-block mt-4">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
